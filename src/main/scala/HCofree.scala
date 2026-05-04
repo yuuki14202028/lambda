86d32344e5333a83986e0a783658180ea1a70135
yuuki14202028 <yuuki14202028@gmail.com>
2026-05-05 01:10:04 +0900

    :recycle: リファクタリング

--- src/main/scala/HCofree.scala
+++ src/main/scala/HCofree.scala
@@ -1,6 +1,8 @@
 package com.yuuki14202028
 
 import cats.arrow.FunctionK
+import cats.Monad
+import cats.syntax.all._
 
 case class HCofree[H[_[_], _], A[_], I](
   head: A[I],
@@ -9,8 +11,13 @@ case class HCofree[H[_[_], _], A[_], I](
 
 type HCofreeAlgebra[H[_[_], _], A[_], B[_]] = [x] => (A[x], H[B, x]) => B[x]
 
+type HCofreeAlgebraM[H[_[_], _], G[_], A[_], B[_]] = [x] => (A[x], H[B, x]) => G[B[x]]
+
 type HCofreeParaAlgebra[H[_[_], _], A[_], B[_]] =
   [x] => (A[x], H[[y] =>> (HCofree[H, A, y], B[y]), x]) => B[x]
+
+type HCofreeParaAlgebraM[H[_[_], _], G[_], A[_], B[_]] =
+  [x] => (A[x], H[[y] =>> (HCofree[H, A, y], B[y]), x]) => G[B[x]]
 
 extension [H[_[_], _], A[_], I](self: HCofree[H, A, I]) {
   def cataAnn[B[_]](alg: HCofreeAlgebra[H, A, B])(using hf: HFunctor[H]): B[I] = {
@@ -18,6 +25,13 @@ extension [H[_[_], _], A[_], I](self: HCofree[H, A, I]) {
       def apply[X](child: HCofree[H, A, X]): B[X] = child.cataAnn(alg)
     })
     alg(self.head, mapped)
+  }
+
+  def cataAnnM[G[_], B[_]](alg: HCofreeAlgebraM[H, G, A, B])(using ht: HTraverse[H], G: Monad[G]): G[B[I]] = {
+    val recursed: G[H[B, I]] = ht.traverse(self.tail)(
+      [x] => (child: HCofree[H, A, x]) => child.cataAnnM(alg)
+    )
+    recursed.flatMap(t => alg(self.head, t))
   }
 
   def paraAnn[B[_]](alg: HCofreeParaAlgebra[H, A, B])(using hf: HFunctor[H]): B[I] = {
@@ -29,5 +43,13 @@ extension [H[_[_], _], A[_], I](self: HCofree[H, A, I]) {
         (child, child.paraAnn(alg))
     })
     alg(self.head, mapped)
+  }
+
+  def paraAnnM[G[_], B[_]](alg: HCofreeParaAlgebraM[H, G, A, B])(using ht: HTraverse[H], G: Monad[G]): G[B[I]] = {
+    type Pair[x] = (HCofree[H, A, x], B[x])
+    val recursed: G[H[Pair, I]] = ht.traverse[G, [x] =>> HCofree[H, A, x], Pair, I](self.tail)(
+      [x] => (child: HCofree[H, A, x]) => child.paraAnnM(alg).map(b => (child, b))
+    )
+    recursed.flatMap(t => alg(self.head, t))
   }
 }
